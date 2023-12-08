7141e5176ff9dad8215363d0523006486ed5cf0f
unsivil <unsivilaudio@gmail.com>
2023-12-08 08:12:26 -0700

    pointers in action

---
+++ advanced/go.mod
@@ -0,0 +1,3 @@
+module advanced-go
+
+go 1.21.5
---
+++ advanced/pointers.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 32
+
+	agePointer := &age
+
+	fmt.Println("Age:", *agePointer)
+
+	editAgeToAdultYears(agePointer)
+	fmt.Println(age)
+}
+
+func editAgeToAdultYears(age *int) {
+	// return *age - 18
+	*age = *age - 18
+}
